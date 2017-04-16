8610c3f0b9e468ae74aad40006e4f3b8db7324a6
santana kenner <sskenner@gmail.com>
2017-04-16 21:40:23 +0000

    Add quote action for form to call API wrapper

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,19 +4,42 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
-  
+
   def show
     @product = Product.find(params[:id])
-  end 
-  
-  def quote 
-    # code for UberRUSH shipping quote
   end
-  
+
+  def quote
+    # code for UberRUSH shipping quote
+    ur = Uber::RUSH.new
+
+    pickup_obj = Uber::PICKUP
+
+    dropoff_obj = {
+      location: {
+        address: params[:address],
+        city: params[:city],
+        state: "New York",
+        postal_code: params[:postal_code],
+        country: "US",
+      }
+    }
+
+    response = ur.delivery_quote(pickup_obj, dropoff_obj)
+
+    respond_to do |format|
+      format.html
+      format.js do
+        @quote = response["quotes"][0]
+        @dropoff = dropoff_obj
+      end
+    end
+  end
+
   def order
     # code for Stripe charge and UberRUSH delivery creation
   end
-  
+
   def done
     # code for confirmation page
   end
