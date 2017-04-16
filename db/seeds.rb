820b4a66bbcd8864e2403358219f35b67d33429e
santana kenner <sskenner@gmail.com>
2017-04-16 14:23:23 +0000

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create(title: 'Nike Flex 2016 RN', description: "Whether you're training for your first race or are just getting back into a gym routine, the Nike Flex 2016 RN will help keep you on stride.", price: 80.00, picture: "http://a1.zassets.com/images/z/3/5/8/8/3/9/3588391-p-MULTIVIEW.jpg")
+Product.create(title: 'PUMA Roma Texture', description: "The classically styled Roma gets a new skin and becomes the Roma Texture!", price: 65.00, picture: "http://a2.zassets.com/images/z/3/6/4/8/2/3/3648235-p-MULTIVIEW.jpg")
+Product.create(title: 'Supra Skytop', description: "Uppers of leather, suede, abrasion-resistant TUF canvas, or duct tape-like TUF materials.", price: 115.00, picture: "http://a3.zassets.com/images/z/3/5/1/8/7/9/3518799-p-MULTIVIEW.jpg") 
+Product.create(title: 'Converse Chuck Taylor Mid', description: "As the seasons change, so do these shoes! Created with kurim, a durable material that changes colors depending on the angle it is viewed from, the Converse Chuck Taylor All Star High Street Mid will catch your eye every step you take. Look forward and live comfortably with Converse.", price: 70.00, picture: "http://a3.zassets.com/images/z/3/5/8/1/8/6/3581860-p-MULTIVIEW.jpg")
+Product.create(title: 'Reebok Crossfit Speed TR', description: "The multi-directional stability of the Reebok Crossfit Speed TR cross-training shoe creates a supportive base to help you exert maximum power and top speeds during even the toughest workout. Be ever ready for that next rep!", price: 99.99, picture: "http://a1.zassets.com/images/z/3/5/9/7/3/4/3597347-p-MULTIVIEW.jpg")
