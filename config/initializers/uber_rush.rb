3bee8b93dc172ea7f3d96938be42f343293e6860
santana kenner <sskenner@gmail.com>
2017-04-16 21:35:52 +0000

    Update API wrapper with pickup location for single store implementation

--- config/initializers/uber_rush.rb
+++ config/initializers/uber_rush.rb
@@ -35,12 +35,12 @@ module Uber
 
       token = self.class.post(url,
         body: {
-          client_id: ENV['uber_rush_client_id'],
-          client_secret: ENV['uber_rush_client_secret'],
+          client_id: ENV['q44Sl2oGyTSE_rc1V1Qy5252oyBwZrNT'],
+          client_secret: ENV['80wPYE04EYRzgYhMQnAZ5HIDslwHJY0WI_K6tcJM'],
           grant_type: 'client_credentials',
           scope: 'delivery'
         }
-      )["access_token"]
+      )["eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOltdLCJzdWIiOiJkZDU1NTI4Zi02ZTUyLTRhMGUtYTg3Ni1lNzNkODMzOWQwY2MiLCJpc3MiOiJ1YmVyLXVzMSIsImp0aSI6IjhiM2Q0NGMzLTk0YjAtNGQ0My1iOGI0LTVmODVjZTE1YWI5MCIsImV4cCI6MTQ5NDk2NzI0NiwiaWF0IjoxNDkyMzc1MjQ2LCJ1YWN0Ijoid1RTWHROSktibUV0M0tCVHhmR2k2bkNvalY0d2MzIiwibmJmIjoxNDkyMzc1MTU2LCJhdWQiOiJxNDRTbDJvR3lUU0VfcmMxVjFReTUyNTJveUJ3WnJOVCJ9.krRJ7jX3hIAufxq0CyxYUSEy8wSUtpqruF1ihWoLJJGnYXvT95zZt4gOn3nnKfLQNG0Qf0ZZNEd_ReqlF7jwPYwoOS27FjB79JFg4uI5zoD5Wv-9UM5gha5n24X4lzBcMgSTk9cjwp1c9Mvi8W8LaE_R1F0rsDh8V5w55-YzChFd5j27EQGio7xaJe0XUGHwjPHB-TdsPc9WtAkt1m_a1zq4NjxClDlkYde41ATo1GF5YA3Dabd8Qr_YD0DPLDT3xTzsapvc-YowMQHN2h3A7WTOsnBrGL8BRAjfGRYqmG0WQTgVAECEYc8iMhq10RdDqIY63xwL4ook74_hYPuPlg"]
 
       @base_path = "?access_token=#{token}"
     end
