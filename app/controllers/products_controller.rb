f048e709e586c08f63d51516312da4deaf3511d2
santana kenner <sskenner@gmail.com>
2017-04-17 18:06:15 +0000

    Add order action for Strip interaction

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,6 +38,56 @@ class ProductsController < ApplicationController
 
   def order
     # code for Stripe charge and UberRUSH delivery creation
+    customer = Stripe::Customer.create(
+      :email => params[:email],
+      :card => params[:stripeToken]
+    )
+
+    ur = Uber::RUSH.new
+    pickup_obj = Uber::PICKUP
+
+    dropoff_obj = {
+      location: {
+        address: params[:address],
+        city: params[:city],
+        state: "New York",
+        postal_code: params[:postal_code],
+        country: "US",
+      },
+      contact: {
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        email: params[:email],
+        phone: {
+          number: "+1" + params[:number],
+          sms_enabled: true
+        }
+      }
+    }
+
+    items = [
+      {
+        title: params[:title],
+        quantity: 1,
+        price: params[:price]
+      }
+    ]
+
+    response = ur.create_delivery(items, pickup_obj, dropoff_obj)
+    @amount = ((params[:price].to_f + response["fee"]) * 100).to_i
+
+    charge = Stripe::Charge.create(
+      :customer => customer.id,
+      :amount => @amount,
+      :description => params[:title],
+      :currency => 'usd'
+    )
+
+    redirect_to done_path(amount: @amount)
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to :back
   end
 
   def done
--- config/initializers/uber_rush.rb
+++ config/initializers/uber_rush.rb
@@ -35,12 +35,12 @@ module Uber
 
       token = self.class.post(url,
         body: {
-          client_id: ENV['uber_rush_client_id'],
-          client_secret: ENV['uber_rush_client_secret'],
+          client_id: ENV['q44Sl2oGyTSE_rc1V1Qy5252oyBwZrNT'],
+          client_secret: ENV['80wPYE04EYRzgYhMQnAZ5HIDslwHJY0WI_K6tcJM'],
           grant_type: 'client_credentials',
           scope: 'delivery'
         }
-      )["access_token"]
+      )["eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOltdLCJzdWIiOiJkZDU1NTI4Zi02ZTUyLTRhMGUtYTg3Ni1lNzNkODMzOWQwY2MiLCJpc3MiOiJ1YmVyLXVzMSIsImp0aSI6Ijc5NDY0ZTIzLTZkZDgtNGFlMy05OTViLTRlNWE1NjM5ZmQwMiIsImV4cCI6MTQ5NTAzODUxNiwiaWF0IjoxNDkyNDQ2NTE2LCJ1YWN0IjoiZ1JBR3RmQm5oekp0ZkRja1NrTUNzYzNydTJ2cmUzIiwibmJmIjoxNDkyNDQ2NDI2LCJhdWQiOiJxNDRTbDJvR3lUU0VfcmMxVjFReTUyNTJveUJ3WnJOVCJ9.KxDOllAHTMci_KShRj4Kmv0HE5xV_4ALbGbVtgK6jpU0mWHGIjzsJtUglHDyNG7rAovvONQIYFOdyTHaasG9-QGHc1UgrMWYzmyCvQQvdJ7TlhAtA1RjJ3BVQBReBVxGwQs-xFncPD3pf_CQ1fyN_Xav5sH5KyltoU2bkumoVU-FYPi2qC2xOLlgk1OcwudiuSDB7An1rEk1StByW-7__n7FLS3z6ywUtNoH4snLkL_-4aD9yLhQaDVqxiIAjEytTe3LkNnIRNznFdv8_pOltmmQOb5NJwfuT0o6RfQ_7IUNBTz2FwThr6ku8cCh7pU05y7JNKi-gBF9ioQBj1A0Qg"]
 
       @base_path = "?access_token=#{token}"
     end
