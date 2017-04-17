2a8fdd515052636b501b85df6c07b7f80c008931
santana kenner <sskenner@gmail.com>
2017-04-17 12:49:42 +0000

    Update Stripe config file with account info

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 # config/initializers/stripe.rb
 
 Rails.configuration.stripe = {
-  publishable_key: ENV['stripe_publishable_key'],
-  secret_key: ENV['stripe_secret_key']
+  publishable_key: ENV['pk_test_PIckIH8IvjCGq80edHBCU08I'],
+  secret_key: ENV['sk_test_JQREcFqcTqOjBp4SfkVTjOQI']
   }
 
   Stripe.api_key = Rails.configuration.stripe[:secret_key]
