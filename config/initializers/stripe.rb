a89d0783ce46e9ff2b5acd304014eaf8aa518551
santana kenner <sskenner@gmail.com>
2017-04-17 12:27:44 +0000

    Add Stripe config file

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,8 @@
+# config/initializers/stripe.rb
+
+Rails.configuration.stripe = {
+  publishable_key: ENV['stripe_publishable_key'],
+  secret_key: ENV['stripe_secret_key']
+  }
+
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]
